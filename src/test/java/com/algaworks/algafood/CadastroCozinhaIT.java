76d486963b16b4e3a1a4e0d91e619729f332609f
Carlos Augusto <augustino2003@hotmail.com>
2022-02-10 01:01:55 -0200

    Configurando um banco de testes usando @TestPropertySource

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -1,15 +1,22 @@
 package com.algaworks.algafood;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import org.flywaydb.core.Flyway;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 class CadastroCozinhaIT {
 	
 //	@Autowired
@@ -18,13 +25,23 @@ class CadastroCozinhaIT {
 	@LocalServerPort
 	private int port;
 	
+	@Autowired
+	private Flyway flyway;
+	
+	@BeforeEach
+	public void setUp() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		RestAssured.port = port;
+		RestAssured.basePath = "/cozinhas";
+		
+		flyway.migrate();
+	}
+	
 	@Test
 	public void deveRetornarStatus200_QuandoConsultarCozinhas() {
-		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
 		
 		RestAssured.given()
-		.basePath("/cozinhas")
-		.port(port)
 		.accept(ContentType.JSON)
 				.when()
 				.get()
@@ -38,14 +55,23 @@ class CadastroCozinhaIT {
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		
 		RestAssured.given()
-		.basePath("/cozinhas")
-		.port(port)
 		.accept(ContentType.JSON)
 				.when()
 				.get()
 				.then()
 				.body("", Matchers.hasSize(4))
 				.body("nome", Matchers.hasItems("Indiana", "Tailandesa"));
+	}
+	
+	@Test
+	public void deveRetornarStatus201_QuandoCadastrarCozinha() {
+		RestAssured.given()
+			.body("{ \"nome\": \"Chinesa\" }")
+			.contentType(ContentType.JSON)
+			.when()
+			.post()
+			.then()
+			.statusCode(HttpStatus.CREATED.value());
 	}
 	
 
