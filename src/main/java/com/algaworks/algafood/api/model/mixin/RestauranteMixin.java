4b6c813a6f06c02a307ad4c37d6fda3ecc6e6f8d
Carlos Augusto <augustino2003@hotmail.com>
2022-03-15 23:28:55 -0300

    Usando @JsonIgnoreProperties e Jackson Mixin

--- src/main/java/com/algaworks/algafood/api/model/mixin/RestauranteMixin.java
+++ src/main/java/com/algaworks/algafood/api/model/mixin/RestauranteMixin.java
@@ -11,26 +11,25 @@ import com.algaworks.algafood.domain.model.Produto;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-public class RestauranteMixin {
+public abstract class RestauranteMixin {
 	
-		
-		@JsonIgnoreProperties(value = "nome", allowGetters = true)
-		private Cozinha cozinha;
-		
-		@JsonIgnore
-		private Endereco endereco;
-		
-		@JsonIgnore
-		private LocalDateTime dataCadastro;
-		
-		@JsonIgnore
-		private LocalDateTime dataAtualizacao;
-		
-		@JsonIgnore
-		private List<FormaPagamento> formasPagamento = new ArrayList<>();
-				
-		@JsonIgnore
-		private List<Produto> produtos = new ArrayList<>();
+	@JsonIgnoreProperties(value = "nome", allowGetters = true)
+	private Cozinha cozinha;
+	
+	@JsonIgnore
+	private Endereco endereco;
+	
+	@JsonIgnore
+	private LocalDateTime dataCadastro;
+	
+	@JsonIgnore
+	private LocalDateTime dataAtualizacao;
+	
+	@JsonIgnore
+	private List<FormaPagamento> formasPagamento = new ArrayList<>();
+			
+	@JsonIgnore
+	private List<Produto> produtos = new ArrayList<>();
 
 
 }
